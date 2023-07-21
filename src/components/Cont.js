85d4670a04732d34977d697dcb0a0d16ed7dc258
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2023-07-21 17:15:44 +0530

    If list is empty

--- src/components/Cont.js
+++ src/components/Cont.js
@@ -34,26 +34,30 @@ function Cont  ()  {
   return (
 <>
 <main>
-     <p>Hi Everyone</p>
-     
-     <ul>
-      {items.map((item) =>(
-        <li className="item" key={item.id}>
-        <input
-         type="checkbox"
-         onChange={() =>handleCheck(item.id)}
-          checked= {item.checked}>
-        </input>
-        <label 
-        style ={(item.checked) ? {textDecoration:'line-through'}: null }
-        onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
-       <FaTrashAlt 
-       role ="button"
-       tabIndex="0"
-       onClick={()=>handleDelete(item.id)}/>
-      </li>
-      ))}
-     </ul>
+<p>Hi Everyone</p>
+  {(items.length) ? (
+  <ul>
+  {items.map((item) =>(
+    <li className="item" key={item.id}>
+    <input
+     type="checkbox"
+     onChange={() =>handleCheck(item.id)}
+      checked= {item.checked}>
+    </input>
+    <label 
+    style ={(item.checked) ? {textDecoration:'line-through'}: null }
+    onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
+   <FaTrashAlt 
+   role ="button"
+   tabIndex="0"
+   onClick={()=>handleDelete(item.id)}/>
+  </li>
+  ))}
+ </ul>
+ ) :(
+  <p>You're list is Empty!!!! </p>
+ )
+ }  
     </main>
     </>
     
