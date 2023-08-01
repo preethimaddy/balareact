cbabeec86fb5b4a69eb4db3070f9112311fea30b
Preethi Madhavan <98338783+preethimaddy@users.noreply.github.com>
2023-08-01 19:09:41 +0530

    get Response from backend

--- src/App.js
+++ src/App.js
@@ -2,17 +2,35 @@ import './App.css';
 import Header from './components/Header';
 import Cont from './components/Cont'
 import Footer from './components/Footer';
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import AddItem from './components/AddItem';
 import Search from './components/Search';
 
 
    function App () {
-    const [items, setItems] = useState(JSON.parse
-      (localStorage.getItem('Todo_list'))
-     )
+
+    const API_URL= 'http://localhost:3500/items'
+
+    const [items, setItems] = useState([])
       const [newItem,setNewItem] = useState()
       const [search, setSearch] = useState('')
+
+      useEffect(()=>{
+        const fetchItems = async() => {
+          try{
+            const response = await fetch(API_URL);
+            console.log(response)
+
+            const listItems =await response.json();
+            console.log(response)
+            setItems(listItems)
+          } catch(err){
+            console.log(err.stack)
+          }
+        }
+        (async () => await fetchItems())()
+      }, [])
+      
       const addItem = (item) =>{
         const id = items.length ? items[items.length -1].id + 1 : 1;
         const addNewItem = {id, checked:false, item}
@@ -23,13 +41,13 @@ import Search from './components/Search';
       const handleCheck = (id) =>{
         const listItems = items.map((item)=> item.id===id ? {...item, checked:!item.checked} : item)
         setItems(listItems)
-        localStorage.setItem('Todo_list', JSON.stringify(listItems))
+        // localStorage.setItem('Todo_list', JSON.stringify(listItems))
       }
   
       const handleDelete = (id) =>{
       const listItems = items.filter((item)=>item.id!==id)
      setItems(listItems)
-     localStorage.setItem('Todo_list', JSON.stringify(listItems))
+    //  localStorage.setItem('Todo_list', JSON.stringify(listItems))
       }
       const handleSubmit=(e) => {
         e.preventDefault();
